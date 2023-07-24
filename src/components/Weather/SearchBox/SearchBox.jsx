5624aadcbebda08e581ddb6757f41894003447f5
karthi-vick <karthid.22mca@kongu.edu>
2023-07-24 16:37:29 +0530

    second

--- src/components/Weather/SearchBox/SearchBox.jsx
+++ src/components/Weather/SearchBox/SearchBox.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import './SearchBox.css'
 import { useState } from 'react'
+import Rain from '../../../Asset/rain.png'
+import Sunny from '../../../Asset/sunny.png'
 function SearchBox() {
-    const [text,setText]=useState("")
-    const handleChange=()=>{
-        setText(e.target.value)
+    const [city,setCity]=useState("")
+    const [weather,setWeather]=useState([{}])
+    const handleChange=(e)=>{
+        setCity(e.target.value)
     }
     const handleClick=(e)=>{
         e.preventDefault();
         const apiKey="6aaffefcd2897eec1631bc4c79ffd80d"
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
-       
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(res=>res.json()).then(
+          data=>{setWeather(data)
+          console.log(data);
+          setCity("")
+          }
+        )
+        
     }
   return (
     <div className='s-value'>
-        
-        <input type="text" value={text} className='s-label' onChange={handleChange} />
+        <div>
+        <input type="text" value={city} className='s-label' onChange={handleChange} />
         <input type="button" value="submit" onClick={handleClick} />
-        
+        </div>
+        <div>{weather.name}</div>
+        {typeof weather.main ==='undefined'?<div>Enter a valid city name</div>:
+        weather.weather[0].main=='Clouds' ?<img src={Sunny}></img> : <img src={Rain}></img>}
     </div>
   )
 }
